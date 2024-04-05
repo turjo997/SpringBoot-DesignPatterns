c21c4e6c9909353e0f879d4bd1db92d603810d8f
turjo997 <turja997@gmail.com>
2024-04-05 10:27:36 +0600

    Saga pattern implementation

---
+++ SAGA Pattern/CommomService/src/main/java/com/spring/CommomService/CommomServiceApplication.java
@@ -0,0 +1,13 @@
+package com.spring.CommomService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommomServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommomServiceApplication.class, args);
+	}
+
+}
---
+++ SAGA Pattern/CommomService/src/test/java/com/spring/CommomService/CommomServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.CommomService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommomServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
