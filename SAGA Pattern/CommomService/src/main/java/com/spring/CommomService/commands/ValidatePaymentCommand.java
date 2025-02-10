5ccb9c9a1162f3be713319000a93b0590711240b
turjo997 <turja997@gmail.com>
2025-02-10 23:03:00 +0600

    wprking

--- SAGA Pattern/CommomService/src/main/java/com/spring/CommomService/commands/ValidatePaymentCommand.java
+++ SAGA Pattern/CommomService/src/main/java/com/spring/CommomService/commands/ValidatePaymentCommand.java
@@ -1,10 +1,17 @@
 package com.spring.CommomService.commands;
 
+import com.spring.CommomService.model.CardDetails;
 import lombok.Builder;
 import lombok.Data;
+import org.axonframework.modelling.command.TargetAggregateIdentifier;
 
 @Data
 @Builder
 public class ValidatePaymentCommand {
+
+    @TargetAggregateIdentifier
+    private String paymentId;
+    private String orderId;
+    private CardDetails cardDetails;
 
 }
--- SAGA Pattern/OrderService/src/main/java/com/spring/OrderService/command/api/saga/OrderProcessingSaga.java
+++ SAGA Pattern/OrderService/src/main/java/com/spring/OrderService/command/api/saga/OrderProcessingSaga.java
@@ -1,10 +1,16 @@
 package com.spring.OrderService.command.api.saga;
 
+import com.spring.CommomService.commands.ValidatePaymentCommand;
+import com.spring.OrderService.command.api.events.OrderCreatedEvent;
 import lombok.extern.slf4j.Slf4j;
 import org.axonframework.commandhandling.gateway.CommandGateway;
+import org.axonframework.modelling.saga.SagaEventHandler;
+import org.axonframework.modelling.saga.StartSaga;
 import org.axonframework.queryhandling.QueryGateway;
 import org.axonframework.spring.stereotype.Saga;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.UUID;
 
 @Saga
 @Slf4j
@@ -15,5 +21,44 @@ public class OrderProcessingSaga {
 
     @Autowired
     private transient QueryGateway queryGateway;
+
+    public OrderProcessingSaga() {
+    }
+
+
+    @StartSaga
+    @SagaEventHandler(associationProperty = "orderId")
+    private void handle(OrderCreatedEvent event) {
+        log.info("OrderCreatedEvent in Saga for Order Id : {}",
+                event.getOrderId());
+
+//        GetUserPaymentDetailsQuery getUserPaymentDetailsQuery
+//                = new GetUserPaymentDetailsQuery(event.getUserId());
+
+//        User user = null;
+//
+//        try {
+//            user = queryGateway.query(
+//                    getUserPaymentDetailsQuery,
+//                    ResponseTypes.instanceOf(User.class)
+//            ).join();
+//
+//        } catch (Exception e) {
+//            log.error(e.getMessage());
+//            //Start the Compensating transaction
+//            cancelOrderCommand(event.getOrderId());
+//        }
+//
+//        ValidatePaymentCommand validatePaymentCommand
+//                = ValidatePaymentCommand
+//                .builder()
+//                .cardDetails(user.getCardDetails())
+//                .orderId(event.getOrderId())
+//                .paymentId(UUID.randomUUID().toString())
+//                .build();
+
+      //  commandGateway.sendAndWait(validatePaymentCommand);
+    }
+
 
 }
